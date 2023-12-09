bbdf1843d3fdca94ee0c5a25aa8956b889069eea
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-12-09 14:06:24 +0100

    Add navigation graph.

---
+++ app/src/main/java/abs/apps/myapplication/ui/navigation/NavigationGraph.kt
@@ -0,0 +1,27 @@
+package abs.apps.myapplication.ui.navigation
+
+import android.annotation.SuppressLint
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun SchorleNavigation(navController: NavHostController = rememberNavController()) {
+    SchorleNavHost(navController = navController)
+}
+
+@Composable
+fun SchorleNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier,
+) {
+/*
+    NavHost(
+        navController = navController,
+        startDestination = StartupDestination.route,
+        modifier = modifier
+*/
+}
