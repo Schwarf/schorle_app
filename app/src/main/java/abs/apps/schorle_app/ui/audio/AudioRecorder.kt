05ddf69d231369c8e6f107bdfe3f594b3bbe9b77
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-12-10 10:37:32 +0100

    Finish recording.

--- app/src/main/java/abs/apps/schorle_app/ui/audio/AudioRecorder.kt
+++ app/src/main/java/abs/apps/schorle_app/ui/audio/AudioRecorder.kt
@@ -2,19 +2,37 @@ package abs.apps.schorle_app.ui.audio
 
 import android.content.Context
 import android.media.MediaRecorder
+import android.os.Build
 import java.io.File
+import java.io.FileOutputStream
 
 class AudioRecorder(private val context: Context) : IAudioRecorder {
 
     private var recorder : MediaRecorder? = null
 
-    private fun createRecorder()
+    private fun createRecorder(): MediaRecorder
+    {
+        return if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
+            MediaRecorder(context)
+        else
+            MediaRecorder()
+    }
     override fun start(outputFile : File)
     {
-
+        createRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+            setOutputFile(FileOutputStream(outputFile).fd)
+            prepare()
+            start()
+            recorder = this
+        }
     }
 
     override fun stop() {
-        TODO("Not yet implemented")
+        recorder?.stop()
+        recorder?.reset()
+        recorder = null
     }
 }
